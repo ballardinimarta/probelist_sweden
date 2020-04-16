6948a992e1b15e3f5426eb8fd95792ff7479f90c
ballardinimarta <marta.ballardini@gmail.com>
2020-04-16 16:13:23 +0200

    added status

--- get_list.py
+++ get_list.py
@@ -1,10 +1,11 @@
 from ripe.atlas.cousteau import ProbeRequest
 from pprint import pprint
+
 filters = {"country_code": "SE"}
 probes = ProbeRequest(**filters)
 
 probelist = []
 for probe in probes:
-    probelist.append([probe['asn_v4'], probe['description'], probe['geometry']['coordinates']])
+    probelist.append([probe['asn_v4'], probe['description'], probe['geometry']['coordinates'], probe['status']])
 
 pprint(probelist)
