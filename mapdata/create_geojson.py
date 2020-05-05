dcb3579a07451e5b40da063c12dab0f46c1e5f0a
ballardinimarta <marta.ballardini@gmail.com>
2020-05-05 15:37:54 +0200

    added info and documentation

--- mapdata/create_geojson.py
+++ mapdata/create_geojson.py
@@ -1,21 +1,29 @@
 import json
 from pprint import pprint
 from urllib.request import urlopen
-from probedict import kommun_dict
 import pandas as pd
+from mapdict import mapdict
 
+# Write a new updated kommun dict to use later at import
+mapdict()
+
+# import the dictionary after it is updated
+from probedict import kommun_dict
+
+# Change the values from ID to the amount of probes
 probes={}
 for x in kommun_dict:
     kommun_dict
     probes[x]=len(kommun_dict[x])
 
+# Make that dictionary a pandas DataFrame
 probe_data = pd.DataFrame.from_dict(probes, orient='index', columns=['antal'])
 
-
+# Get the geojsonfile with the polygons of swedish municipalities
 with urlopen('http://kodapan.se/geodata/data/2015-06-26/kommuner-kustlinjer.geo.json') as res:
     kommuner = json.load(res)
 
-
+# Append the ammount of probes for every municipaliy in to the geojson file
 for x in kommuner['features']:
     for k in probe_data.index:
         if k in x['properties']['name']:
@@ -27,6 +35,9 @@ for x in kommuner['features']:
     except KeyError:
         x['properties']['probes'] = '0'
 
+# Print to the already exisiting geojson file in the same folder
+def geojson_print(kommuner):
+    with open('mapdata/sveriges_kommuner.geojson', 'w') as skg:
+        print('kommuner_geojson = {}'.format(kommuner), file = skg)
 
-
-print('kommuner_geojson = {}'.format(kommuner) )
+geojson_print(kommuner)
--- mapdata/mapdict.py
+++ mapdata/mapdict.py
@@ -2,84 +2,74 @@ from geopy.geocoders import Nominatim
 from ripe.atlas.cousteau import ProbeRequest
 from pprint import pprint
 
+def mapdict():
+    # Filters for all connected and disconncted probes
+    filters_1 = {"country_code": "SE", "status": "1"}
+    filters_2 = {"country_code": "SE", "status": "2"}
 
-filters_1 = {"country_code": "SE", "status": "1"}
-filters_2 = {"country_code": "SE", "status": "2"}
-probes_con = ProbeRequest(**filters_1)
-probes_dis_con = ProbeRequest(**filters_2)
+    # Get the probes from those filters
+    probes_con = ProbeRequest(**filters_1)
+    probes_dis_con = ProbeRequest(**filters_2)
 
+    # Take out coordinates and ids to separate lists
+    probe_id_list = []
+    coordinatelist=[]
+    for probe in probes_con:
+        coordinatelist.append(probe['geometry']['coordinates'])
+        probe_id_list.append(probe['id'])
+    for probe in probes_dis_con:
+        coordinatelist.append(probe['geometry']['coordinates'])
+        probe_id_list.append(probe['id'])
 
-probe_id_list = []
-coordinatelist=[]
-for probe in probes_con:
-    coordinatelist.append(probe['geometry']['coordinates'])
-    probe_id_list.append(probe['id'])
-for probe in probes_dis_con:
-    coordinatelist.append(probe['geometry']['coordinates'])
-    probe_id_list.append(probe['id'])
+    # Flip the order so its Lat, Lon
+    flip_list=[]
+    for coordinate in coordinatelist:
+        lon=str(coordinate[0])
+        lat=str(coordinate[1])
+        coordinates= (lat + ', ' + lon)
+        flip_list.append(coordinates)
+    # Set geolocator
+    geolocator = Nominatim(user_agent="probelist_sweden", timeout=None)
 
-try:
-    oslo_probe = probe_id_list.index(3452)
-except ValueError:
-    pass
-else:
-    probe_id_list.remove(probe_id_list[oslo_probe])
-    coordinatelist.remove(coordinatelist[oslo_probe])
-try:
-    lilla_vartan = probe_id_list.index(31950)
-except ValueError:
-    pass
-else:
-    probe_id_list.remove(probe_id_list[lilla_vartan])
-    coordinatelist.remove(coordinatelist[lilla_vartan])
-
-
-flip_list=[]
-for coordinate in coordinatelist:
-    lon=str(coordinate[0])
-    lat=str(coordinate[1])
-    coordinates= (lat + ', ' + lon)
-    flip_list.append(coordinates)
-
-geolocator = Nominatim(user_agent="probelist_sweden", timeout=None)
-
-
-location_list=[]
-coordinates = flip_list
-for coor in coordinates:
-    location = geolocator.reverse(coor)
-    if 'address' in location.raw:
-        if 'municipality' in location.raw['address']:
-            kommun = location.raw['address']['municipality']
-            location_list.append(kommun)
-        elif 'town' in location.raw['address']:
-            town = location.raw['address']['town']
-            location_list.append(town)
+    # Get the municipality of the coordinates
+    # Some off adresses does not have a stated municipality but the 'town' key instead has the municipality information
+    location_list=[]
+    coordinates = flip_list
+    for coor in coordinates:
+        location = geolocator.reverse(coor)
+        if 'address' in location.raw:
+            if 'municipality' in location.raw['address']:
+                kommun = location.raw['address']['municipality']
+                location_list.append(kommun)
+            elif 'town' in location.raw['address']:
+                town = location.raw['address']['town']
+                location_list.append(town)
 
 
-keys = probe_id_list
-values = location_list
+    keys = probe_id_list
+    values = location_list
 
+    # Make the list a dictionary
+    location_dict = {k: v for k, v in zip(keys, values)}
 
-location_dict = {k: v for k, v in zip(keys, values)}
+    # Make a new dict where municipality is key and ids are the values
+    kommun_dict = {}
+    for key, value in location_dict.items():
+        if value not in kommun_dict:
+            kommun_dict[value] = [key]
+        else:
+            kommun_dict[value].append(key)
 
-kommun_dict = {}
-for key, value in location_dict.items():
-    if value not in kommun_dict:
-        kommun_dict[value] = [key]
-    else:
-        kommun_dict[value].append(key)
+    # 'Göteborgs kommun' is stated as 'Göteborgs Stad' so we change that for later on
+    for x in list(kommun_dict):
+        if 'Göteborgs Stad' in x:
+            kommun_dict['Göteborgs kommun'] = kommun_dict.pop('Göteborgs Stad')
 
-for x in list(kommun_dict):
-    if 'Göteborgs Stad' in x:
-        kommun_dict['Göteborgs kommun'] = kommun_dict.pop('Göteborgs Stad')
+    # Print it in the form of a dictionary that is called 'kommun_dict' and print the output to a file called 'probedict.py'
+    def dict_print(kommun_dict):
+        with open('mapdata/probedict.py', 'w') as pbd:
+            print('kommun_dict={}'.format(kommun_dict), file=pbd)
 
-def print_nice(kommun_dict):
-    for kommun, id in kommun_dict.items():
-        print("{}:\n {}\n".format(kommun, id))
-    print('Antal kommuner : %d/290' % len(kommun_dict))
+    dict_print(kommun_dict)
 
-def dict_print(kommun_dict):
-    print('kommun_dict={}'.format(kommun_dict))
-
-dict_print(kommun_dict)
+mapdict()
